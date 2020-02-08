37c73772622edc0a7f8aac129d019ed0c9c40ca0
Shashank Neelagiri <sneelagiri@outlook.com>
2020-02-08 11:55:09 +0100

    added message limit logic

--- messages-api.js
+++ messages-api.js
@@ -5,8 +5,21 @@ const port = 3000;
 app.use(express.json());
 
 app.post("/messages", (req, res) => {
-  console.log(req.body.text);
-  res.json({ message: "I received your request body!" });
+  if (
+    (req.body.text === undefined || req.body.text.length === 0) &&
+    badRequestCount < 5
+  ) {
+    ++badRequestCount;
+    console.log(badRequestCount);
+    res.status(400).end();
+  } else if (badRequestCount >= 5) {
+    // This if statement purposefully shows Too Many Requests even if a request is a "good" request
+    res.status(429).end();
+    setTimeout(resetCount, 300000);
+  } else {
+    console.log(req.body.text);
+    res.json({ message: "I received your request body!" });
+  }
 });
 
 app.listen(port, () => {
