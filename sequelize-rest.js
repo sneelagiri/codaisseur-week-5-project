8376ca4876dd01e66c029bb606ac8a7ff6a9bdb3
Shashank Neelagiri <sneelagiri@outlook.com>
2020-02-08 13:46:41 +0100

    added 3 rows of example data to database

--- sequelize-rest.js
+++ sequelize-rest.js
@@ -29,4 +29,21 @@ db.sync()
   })
   .catch(console.error);
 
+Movie.create({
+  title: "Life of Pi",
+  yearOfRelease: 2012,
+  synopsis: "Boy shares a boat with a lion in the middle of a sea"
+});
+Movie.create({
+  title: "Slumdog Millionaire",
+  yearOfRelease: 2008,
+  synopsis: "Man wins competition because of tragic memories"
+});
+Movie.create({
+  title: "My Name is Khan",
+  yearOfRelease: 2010,
+  synopsis:
+    "Man's son dies because of discrimination post 9/11. Man embarks on a journey to change society."
+});
+
 module.exports = db;
