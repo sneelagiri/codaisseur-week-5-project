af37c4c99f7f042aac221e01768a3bb329841773
Shashank Neelagiri <sneelagiri@outlook.com>
2020-02-08 11:52:41 +0100

    added body parser and single endpoint

--- messages-api.js
+++ messages-api.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+app.post("/messages", (req, res) => {});
+
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
 });
