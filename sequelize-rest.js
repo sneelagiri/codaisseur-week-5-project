c50900fd3b20305bcf2de789c502321290b1a674
Shashank Neelagiri <sneelagiri@outlook.com>
2020-02-08 13:31:19 +0100

    defined Movie model using Sequelize

--- sequelize-rest.js
+++ sequelize-rest.js
@@ -3,6 +3,26 @@ const databaseUrl =
   process.env.DATABASE_URL ||
   "postgres://postgres:secret@localhost:5432/postgres"; //Created new container in Docker
 const db = new Sequelize(databaseUrl);
+
+const Movie = db.define(
+  "movie",
+  {
+    title: {
+      type: Sequelize.STRING,
+      field: "movie_title"
+    },
+    yearOfRelease: {
+      type: Sequelize.INTEGER,
+      field: "movie_year"
+    },
+    synopsis: {
+      type: Sequelize.STRING,
+      field: "movie_synopsis"
+    }
+  },
+  { tableName: "movies" }
+);
+
 db.sync()
   .then(() => {
     console.log("Schema will be updated here");
